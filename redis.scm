e55bf27ca33f53d7790346abb14b5556175e000f
Andrei Barbu <andrei@0xab.com>
2012-12-23 01:07:15 -0500

    Wrap up the context pointer

--- redis.scm
+++ redis.scm
@@ -1,10 +1,12 @@
 (module redis (redis-connect redis-command)
 (import chicken scheme extras foreign)
-(use traversal bind easyffi lolevel)
+(use traversal bind easyffi lolevel define-structure)
 
 #>
 #include "hiredis/hiredis.h"
 <#
+
+(define-structure redis-context ptr)
 
 (define redis-err-io (foreign-value "REDIS_ERR_IO" int))
 (define redis-err-eof (foreign-value "REDIS_ERR_EOF" int))
@@ -21,7 +23,7 @@
 
 (bind "#include \"hiredis-proto.h\"")
 
-(define (process-hiredis-reply context reply)
+(define (process-hiredis-reply context-ptr reply)
  (when reply
   (cond ((equal? redis-reply-string (redisReply-type reply))
          (redisReply-str reply))
@@ -29,7 +31,7 @@
          (map-n
            (lambda (i)
             (process-hiredis-reply
-             context
+             context-ptr
              ((foreign-lambda* (c-pointer "redisReply")
                                (((c-pointer "redisReply") reply)
                                 (integer i))
@@ -43,27 +45,28 @@
          (error "redis status unimplemented"))
         ((equal? redis-reply-error (redisReply-type reply))
          (cond
-          ((equal? redis-err-io (redisContext-err context))
+          ((equal? redis-err-io (redisContext-err context-ptr))
            (error "redis I/O error"))
-          ((equal? redis-err-eof (redisContext-err context))
+          ((equal? redis-err-eof (redisContext-err context-ptr))
            (error "redis unexpected end of file"))
-          ((equal? redis-err-protocol (redisContext-err context))
+          ((equal? redis-err-protocol (redisContext-err context-ptr))
            (error "redis protocol error"))
-          ((equal? redis-err-oom (redisContext-err context))
+          ((equal? redis-err-oom (redisContext-err context-ptr))
            (error "redis out of memory"))
-          ((equal? redis-err-other (redisContext-err context))
-           (error (string-append "redis " (redisContext-errstr context))))
+          ((equal? redis-err-other (redisContext-err context-ptr))
+           (error (string-append "redis " (redisContext-errstr context-ptr))))
           (else (error "redis unknown error" (redisReply-type reply)))))
         (else (error "unexpected redis reply" (redisReply-type reply))))))
 
 (define (redis-command context command)
+ (unless (redis-context? context) (error "invalid redis context" context))
  (process-hiredis-reply
-  context
+  (redis-context-ptr context)
   (set-finalizer! ((foreign-lambda (c-pointer "redisReply")
                                    "redisCommand"
                                    (c-pointer "redisContext")
                                    c-string)
-                   context
+                   (redis-context-ptr context)
                    command) 
                   freeReplyObject)))
 
@@ -73,5 +76,5 @@
   (set-finalizer! context redisFree)
   (when (> (redisContext-err context) 0)
    (error (string-append "redis " (redisContext-errstr context))))
-  context))
+  (make-redis-context context)))
 )
